269d5c5fa2fb78408e958220a6af43c1bbcb9f1a
Kenny Truong <kennytruong@Kennys-MacBook-Air.local>
2022-02-08 10:02:36 -0800

    Completed pcost.py

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,16 @@
 # pcost.py
 #
 # Exercise 1.27
+runningTotal = 0.0
+with open('Data/portfolio.csv','rt') as file: # Open the file, if leaving the block it will automatically close
+    headers = next(file) # Skip the first line in the file
+
+    for line in file: # for each line in the file
+        print(line, end='') # print the line
+        row = line.split(',') # Then split the line into a list
+        runningTotal += float(row[1]) * float(row[2]) # Num shares * cost of each share added into a running total
+
+    print('Total:',runningTotal) # print running total
+
+
+
